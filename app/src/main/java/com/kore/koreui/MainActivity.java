faffc1bac4c9c898f9b1c85fc6c865557c99ca09
SudheerJa-Kore <sudheer.jampana@kore.com>
2023-08-23 18:15:30 +0530

    config_update_added

--- app/src/main/java/com/kore/koreui/MainActivity.java
+++ app/src/main/java/com/kore/koreui/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import kore.botssdk.activity.BotChatActivity;
 import kore.botssdk.net.SDKConfiguration;
+import kore.botssdk.net.SdkConfig;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnBotConnect = findViewById(R.id.btnBotConnect);
-        SDKConfiguration.setCustomTemplateView("link", new LinkTemplateView(MainActivity.this));
-        SDKConfiguration.setCustomTemplateView("button", new BotButtonView(MainActivity.this));
+
+        SdkConfig.setCustomTemplateView("link", new LinkTemplateView(MainActivity.this));
+        SdkConfig.setCustomTemplateView("button", new BotButtonView(MainActivity.this));
+
+        SdkConfig.intialize("st-b9889c46-218c-58f7-838f-73ae9203488c", "Sudheer Bot", "cs-1e845b00-81ad-5757-a1e7-d0f6fea227e9", "5OcBSQtH/k6Q/S6A3bseYfOee02YjjLLTNoT1qZDBso=", "anilkumar.routhu@kore.com");
+
         btnBotConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
