c7de8f3150df201ff27641881dbc9cf2137da01c
SudheerJa-Kore <sudheer.jampana@kore.com>
2023-08-25 20:24:25 +0530

    korbotsdk_lib_changes

--- app/src/main/java/com/kore/koreui/MainActivity.java
+++ app/src/main/java/com/kore/koreui/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import kore.botssdk.activity.BotChatActivity;
-import kore.botssdk.net.SDKConfiguration;
+//import kore.botssdk.net.SDKConfiguration;
 import kore.botssdk.net.SdkConfig;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnBotConnect = findViewById(R.id.btnBotConnect);
 
-        SdkConfig.setCustomTemplateView("link", new LinkTemplateView(MainActivity.this));
-        SdkConfig.setCustomTemplateView("button", new BotButtonView(MainActivity.this));
+//        SdkConfig.setCustomTemplateView("link", new LinkTemplateView(MainActivity.this));
+//        SdkConfig.setCustomTemplateView("button", new BotButtonView(MainActivity.this));
 
         SdkConfig.intialize("st-b9889c46-218c-58f7-838f-73ae9203488c", "Sudheer Bot", "cs-1e845b00-81ad-5757-a1e7-d0f6fea227e9", "5OcBSQtH/k6Q/S6A3bseYfOee02YjjLLTNoT1qZDBso=", "anilkumar.routhu@kore.com");
 
